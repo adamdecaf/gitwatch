64b2825774110cd8d140c4d0a6a9c1c5f41aa1f7
Adam Shannon <adamkshannon@gmail.com>
2018-02-17 13:15:33 -0600

    add test for .head()

--- parse_test.go
+++ parse_test.go
@@ -5,14 +5,31 @@ import (
 )
 
 func TestGitWatch__parseRepoName(t *testing.T) {
-	repo := parseRepoName("https://github.com/adamdecaf/cert-manage.git")
+	repo := parseRepoName("https://github.com/adamdecaf/gitwatch.git")
 	if repo == nil {
 		t.Fatal("expected result")
 	}
 	if repo.host != "github.com" {
 		t.Errorf("got %q", repo.host)
 	}
-	if repo.localpath != "github.com/adamdecaf/cert-manage" {
+	if repo.localpath != "github.com/adamdecaf/gitwatch" {
 		t.Errorf("got %q", repo.localpath)
+	}
+
+	// storagePath
+	storagePath := repo.storagePath()
+	if storagePath != ".storage/github.com/adamdecaf/gitwatch" {
+		t.Errorf("got %q", storagePath)
+	}
+}
+
+func TestGitWatch__head(t *testing.T) {
+	repo := parseRepoName("https://github.com/adamdecaf/gitwatch.git")
+	if repo == nil {
+		t.Fatal("expected result")
+	}
+
+	if ref, _ := repo.head(); ref == "" {
+		t.Error("expected commit")
 	}
 }
